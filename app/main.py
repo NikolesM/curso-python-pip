93f34045462d31af22e4cbbb89979b290a9d96d8
NikolesM <nicolasmantilla06@gmail.com>
2025-04-25 10:25:03 -0500

    Consolidacion y fin de proyecto

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
 import utils_ejercicio
 import read_csv
 import charts
+import pandas as pd
 
 def run():
-  data = read_csv.read_csv('world_population.csv')
+  
+  
+  '''
+  
   
   data = list(filter(lambda item : item['Continent'] == 'South America',data))
 
@@ -11,7 +15,15 @@ def run():
   percentages = list(map(lambda x: x['World Population Percentage'], data))
   charts.generate_pie_chart(countries, percentages)
   
+  ''' 
   
+  df = pd.read_csv('world_population.csv')
+  df = df[df['Continent'] == 'Africa']
+  countries = df['Country/Territory'].values
+  percentages = df['World Population Percentage'].values
+  charts.generate_pie_chart(countries, percentages)
+
+  data = read_csv.read_csv('world_population.csv')
   country = input('Type Country => ')
   print(country)
 
